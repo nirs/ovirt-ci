857f949d17dcd6b3cd985abe1b55aa3c6444affc
Nir Soffer <nsoffer@redhat.com>
2019-03-15 20:28:35 +0200

    Add quick and dirty system-tests test script
    The script implements building artifacts and running oVirt system tests
    basic suite with the built artifacts.

---
+++ system-tests.py
@@ -0,0 +1,71 @@
+import logging
+import os
+import sys
+
+from six.moves import configparser
+
+from oci import gerrit
+from oci import jenkins
+
+logging.basicConfig(
+    level=logging.INFO,
+    format="%(asctime)s %(levelname)-7s [%(name)s] %(message)s")
+
+log = logging.getLogger("system-tests")
+
+change = sys.argv[1]
+
+if len(sys.argv) > 2:
+    engine_version = sys.argv[2]
+else:
+    engine_version = "master"
+
+suite_type = "basic"
+
+config = configparser.ConfigParser()
+conf_path =  os.path.expanduser("~/.config/oci.conf")
+config.read(conf_path)
+
+ga = gerrit.API(host=config.get("gerrit", "host"))
+
+ja = jenkins.API(
+    host=config.get("jenkins", "host"),
+    user_id=config.get("jenkins", "user_id"),
+    api_token=config.get("jenkins", "api_token"))
+
+log.info("[ 1/8 ] Getting build info for change %s", change)
+info = ga.build_info(change)
+
+log.info("[ 2/8 ] Starting build-artifacts job for %s", info)
+queue_url = ja.run(
+    url=info["url"], ref=info["ref"], stage="build-artifacts")
+
+log.info("[ 3/8 ] Waiting for queue item %s", queue_url)
+job_url = ja.wait_for_queue(queue_url)
+
+log.info("[ 4/8 ] Waiting for job %s", job_url)
+result = ja.wait_for_job(job_url)
+
+if result != "SUCCESS":
+    log.error("Build artifacts %s failed", job_url)
+    sys.exit(1)
+
+log.info("[ 5/8 ] Starting oVirt system tests %s suite with custom "
+         "repos %s",
+         suite_type, job_url)
+queue_url = ja.build(
+    "ovirt-system-tests_manual",
+    parameters={
+        "CUSTOM_REPOS": job_url,
+        "ENGINE_VERSION": engine_version,
+        "SUITE_TYPE": suite_type,
+    }
+)
+
+log.info("[ 6/8 ] Waiting for queue item %s", queue_url)
+job_url = ja.wait_for_queue(queue_url)
+
+log.info("[ 7/8 ] Waiting for job %s", job_url)
+result = ja.wait_for_job(job_url)
+
+log.info("[ 8/8 ] System tests completed with %s", result)
