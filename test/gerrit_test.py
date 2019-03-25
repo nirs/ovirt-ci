fe0ecfffce25d2fafeea31e512438efd8b9a0658
Funkabell <blevin@redhat.com>
2019-03-25 18:54:46 +0200

    Add a stable source for gerrit tests
    Patch https://gerrit.ovirt.org/#/c/98719/ was merged thus no
    changes will be made to the tested values.

--- test/gerrit_test.py
+++ test/gerrit_test.py
@@ -3,9 +3,9 @@ from oci import gerrit
 
 def test_build_info():
     ga = gerrit.API("gerrit.ovirt.org")
-    res = ga.build_info("96774")
+    res = ga.build_info("98719")
 
     assert res == {
-        "ref": "refs/changes/74/96774/7",
+        "ref": "refs/changes/19/98719/2",
         "url": "git://gerrit.ovirt.org/vdsm",
     }
