6cd01c02f790e50a16123695b0d3b757844dadf5
Benny Zlotnik <bzlotnik@redhat.com>
2019-03-27 15:54:52 +0200

    introduce colored_output
    Instead of using the logger module to output information
    to the user, we use the colored_output model which simply outputs
    the information to stdout.
    If stdout is a tty it will use beatiful colors to make it easy on
    the eye
    
    Fixes #14

--- build-artifacts.py
+++ build-artifacts.py
@@ -4,6 +4,7 @@ import sys
 from oci import config
 from oci import gerrit
 from oci import jenkins
+from oci import output
 
 logging.basicConfig(
     level=logging.INFO,
@@ -22,17 +23,21 @@ ja = jenkins.API(
     user_id=cfg.jenkins.user_id,
     api_token=cfg.jenkins.api_token)
 
-log.info("[ 1/5 ] Getting build info for change %s", change)
+output.info("[ 1/5 ] Getting build info for change %s", change)
 info = ga.build_info(change)
 
-log.info("[ 2/5 ] Starting build-artifacts job for %s", info)
+output.info("[ 2/5 ] Starting build-artifacts job for %s", info)
 queue_url = ja.run(
     url=info["url"], ref=info["ref"], stage="build-artifacts")
 
-log.info("[ 3/5 ] Waiting for queue item %s", queue_url)
+output.info("[ 3/5 ] Waiting for queue item %s", queue_url)
 job_url = ja.wait_for_queue(queue_url)
 
-log.info("[ 4/5 ] Waiting for job %s", job_url)
+output.info("[ 4/5 ] Waiting for job %s", job_url)
 result = ja.wait_for_job(job_url)
 
-log.info("[ 5/5 ] Job completed with %s", result)
+if result != "SUCCESS":
+    output.failure("Build artifcats failed with %s", result)
+    sys.exit(1)
+
+output.success("[ 5/5 ] Job completed successfuly, congragulations!")
--- system-tests.py
+++ system-tests.py
@@ -4,12 +4,13 @@ import sys
 from oci import config
 from oci import gerrit
 from oci import jenkins
+from oci import output
 
 logging.basicConfig(
     level=logging.INFO,
     format="%(asctime)s %(levelname)-7s [%(name)s] %(message)s")
 
-log = logging.getLogger("system-tests")
+logging = logging.getLogger("system-tests")
 
 change = sys.argv[1]
 
@@ -29,24 +30,24 @@ ja = jenkins.API(
     user_id=cfg.jenkins.user_id,
     api_token=cfg.jenkins.api_token)
 
-log.info("[ 1/8 ] Getting build info for change %s", change)
+output.info("[ 1/8 ] Getting build info for change %s", change)
 info = ga.build_info(change)
 
-log.info("[ 2/8 ] Starting build-artifacts job for %s", info)
+output.info("[ 2/8 ] Starting build-artifacts job for %s", info)
 queue_url = ja.run(
     url=info["url"], ref=info["ref"], stage="build-artifacts")
 
-log.info("[ 3/8 ] Waiting for queue item %s", queue_url)
+output.info("[ 3/8 ] Waiting for queue item %s", queue_url)
 job_url = ja.wait_for_queue(queue_url)
 
-log.info("[ 4/8 ] Waiting for job %s", job_url)
+output.info("[ 4/8 ] Waiting for job %s", job_url)
 result = ja.wait_for_job(job_url)
 
 if result != "SUCCESS":
-    log.error("Build artifacts %s failed", job_url)
+    output.fail("Build artifacts %s failed", job_url)
     sys.exit(1)
 
-log.info("[ 5/8 ] Starting oVirt system tests %s suite with custom "
+output.info("[ 5/8 ] Starting oVirt system tests %s suite with custom "
          "repos %s",
          suite_type, job_url)
 queue_url = ja.build(
@@ -58,10 +59,14 @@ queue_url = ja.build(
     }
 )
 
-log.info("[ 6/8 ] Waiting for queue item %s", queue_url)
+output.info("[ 6/8 ] Waiting for queue item %s", queue_url)
 job_url = ja.wait_for_queue(queue_url)
 
-log.info("[ 7/8 ] Waiting for job %s", job_url)
+output.info("[ 7/8 ] Waiting for job %s", job_url)
 result = ja.wait_for_job(job_url)
 
-log.info("[ 8/8 ] System tests completed with %s", result)
+if result != "SUCCESS":
+    output.failure("System tests failed with: %s", result)
+    sys.exit(1)
+
+output.success("System tests completed successfully, congratulations!")
