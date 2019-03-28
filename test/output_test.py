384858e651df585b553f410d3b7882d4b50a3f5f
Nir Soffer <nsoffer@redhat.com>
2019-03-28 20:20:10 +0200

    Remove unneeded monkeypatching from output test
    When using capsys, we can patch sys.stdout safely, since capsys fixture
    will restore the original file object for us.

--- test/output_test.py
+++ test/output_test.py
@@ -9,15 +9,15 @@ def test_info(capsys):
     assert captured.out == "msg\n"
 
 
-def test_success(monkeypatch, capsys):
-    monkeypatch.setattr(sys.stdout, "isatty", lambda: True)
+def test_success(capsys):
+    sys.stdout.isatty = lambda: True
     output.success("msg")
     captured = capsys.readouterr()
     assert captured.out == output.GREEN + "msg" + output.RESET + "\n"
 
 
-def test_failure(monkeypatch, capsys):
-    monkeypatch.setattr(sys.stdout, "isatty", lambda: True)
+def test_failure(capsys):
+    sys.stdout.isatty = lambda: True
     output.failure("msg")
     captured = capsys.readouterr()
     assert captured.out == output.RED + "msg" + output.RESET + "\n"
@@ -29,8 +29,8 @@ def test_color_no_tty(capsys):
     assert captured.out == "msg\n"
 
 
-def test_color_with_args(monkeypatch, capsys):
-    monkeypatch.setattr(sys.stdout, "isatty", lambda: True)
+def test_color_with_args(capsys):
+    sys.stdout.isatty = lambda: True
     output.success("msg %s %s", "1", "2")
     captured = capsys.readouterr()
     assert captured.out == output.GREEN + "msg 1 2" + output.RESET + "\n"
