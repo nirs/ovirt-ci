115996339666c91114bb8a6e58783c55a309ac4a
Nir Soffer <nsoffer@redhat.com>
2019-03-28 20:05:19 +0200

    Remove unneeded constants from output test
    Having the result of the test in the test make it easier to work with.

--- test/output_test.py
+++ test/output_test.py
@@ -2,10 +2,6 @@ import pytest
 import sys
 from oci import output
 
-GREEN_OUTPUT = output.GREEN + "msg" + output.RESET + "\n"
-RED_OUTPUT = output.RED + "msg" + output.RESET + "\n"
-GREEN_OUTPUT_WITH_ARGS = output.GREEN + "msg 1 2" + output.RESET + "\n"
-
 
 def test_info(capsys):
     output.info("msg")
@@ -17,14 +13,14 @@ def test_success(monkeypatch, capsys):
     monkeypatch.setattr(sys.stdout, "isatty", lambda: True)
     output.success("msg")
     captured = capsys.readouterr()
-    assert captured.out == GREEN_OUTPUT
+    assert captured.out == output.GREEN + "msg" + output.RESET + "\n"
 
 
 def test_failure(monkeypatch, capsys):
     monkeypatch.setattr(sys.stdout, "isatty", lambda: True)
     output.failure("msg")
     captured = capsys.readouterr()
-    assert captured.out == RED_OUTPUT
+    assert captured.out == output.RED + "msg" + output.RESET + "\n"
 
 
 def test_color_no_tty(capsys):
@@ -37,4 +33,4 @@ def test_color_with_args(monkeypatch, capsys):
     monkeypatch.setattr(sys.stdout, "isatty", lambda: True)
     output.success("msg %s %s", "1", "2")
     captured = capsys.readouterr()
-    assert captured.out == GREEN_OUTPUT_WITH_ARGS
+    assert captured.out == output.GREEN + "msg 1 2" + output.RESET + "\n"
