51129d1c99e3d8ee8eae098e40286c634e287644
Bell Levin <blevin@redhat.com>
2019-04-16 18:29:39 +0300

    Add system-tests subcommand
    Add new system-tests sub command, and remove the system-tests.py script.
    
    Example usage:
    
        ./ovirt-ci system-tests 54321
    
    Resolves #7.

--- oci/main.py
+++ oci/main.py
@@ -27,6 +27,22 @@ def run():
         'change',
         help='Gerrit change number')
 
+    system_tests_parser = subparsers.add_parser(
+        "system-tests",
+        help="run system tests for a change")
+    system_tests_parser.set_defaults(command=system_tests)
+
+    system_tests_parser.add_argument(
+        'change',
+        help='Gerrit change number')
+
+    system_tests_parser.add_argument(
+        '--engine-version',
+        help='Supported versions can be found at '
+             'https://jenkins.ovirt.org/job/ovirt-system-tests_manual/build '
+             'under "ENGINE_VERSION"',
+        default='master')
+
     args = parser.parse_args()
 
     logging.basicConfig(
@@ -34,6 +50,71 @@ def run():
         format="%(asctime)s %(levelname)-7s [%(name)s] %(message)s")
 
     args.command(args)
+
+
+def system_tests(args):
+    cfg = config.load()
+
+    suite_type = "basic"
+
+    ga = gerrit.API(host=cfg.gerrit.host)
+
+    ja = jenkins.API(
+        host=cfg.jenkins.host,
+        user_id=cfg.jenkins.user_id,
+        api_token=cfg.jenkins.api_token)
+
+    out = output.TextOutput(steps=8)
+
+    # TODO: the build-artifacts flow was copied from build-artifacts.py. Find a
+    # way to reuse the code instead of copying it.
+
+    out.step("Getting build info for change %s", args.change)
+    info = ga.build_info(args.change)
+
+    out.step("Starting build-artifacts job")
+    out.info(("project", info["project"]),
+             ("branch", info["branch"]),
+             ("patchset", info["patchset"]))
+    queue_url = ja.run(
+        url=info["url"], ref=info["ref"], stage="build-artifacts")
+
+    out.step("Waiting until job is executed")
+    out.info(("queue", queue_url))
+    job_url = ja.wait_for_queue(queue_url)
+
+    out.step("Waiting until job is completed")
+    out.info(("job", job_url))
+    result = ja.wait_for_job(job_url)
+
+    if result != "SUCCESS":
+        out.failure("Build artifacts %s failed", job_url)
+        sys.exit(1)
+
+    out.step("Starting oVirt system tests job")
+    out.info(("suite", suite_type), ("repo", job_url))
+    queue_url = ja.build(
+        "ovirt-system-tests_manual",
+        parameters={
+            "CUSTOM_REPOS": job_url,
+            "ENGINE_VERSION": args.engine_version,
+            "SUITE_TYPE": suite_type,
+        }
+    )
+
+    out.step("Waiting until job is executed")
+    out.info(("queue", queue_url))
+    job_url = ja.wait_for_queue(queue_url)
+
+    out.step("Waiting until job is completed")
+    out.info(("job", job_url))
+    result = ja.wait_for_job(job_url)
+
+    if result != "SUCCESS":
+        out.failure("System tests failed with: %s", result)
+        sys.exit(1)
+
+    out.success("System tests completed successfully, congratulations!")
 
 
 def build_artifacts(args):
